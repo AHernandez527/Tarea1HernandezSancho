7ca8ceb12bf2f60c0699998c425c346bbff1723a
AHernandez527 <79866739+AHernandez527@users.noreply.github.com>
2021-03-04 01:38:50 -0600

    Add files via upload
    Programa que contiene errores detectables con Flake8.

---
+++ programa3_micros.py
@@ -0,0 +1,11 @@
+# Codigo con errores detectables por Flake8
+
+lado=int  (input("Digite el lado del cuadrado: "))
+Area=lado*lado#Aqui se calcula el área
+print("El area de un cuadrado de calcula elevando al cuadrado el valor del lado, es decir, multiplicándolo por sí mismo, lo que da un resultado de:", Area)
+
+
+    
+# Los erroes detectables son la extensión de las líneas, la falta de espacios
+# antes y después de los operadores, espacios antes de los paréntesis,
+# exceso de lineas blancas y espaciado en los comentarios.
