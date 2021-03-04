4b533b1952ecfd2c33ee29e1558df4f57d38183f
AHernandez527 <79866739+AHernandez527@users.noreply.github.com>
2021-03-04 01:24:57 -0600

    Add files via upload
    Este programa es la versión corregida del programa 3 que contenía errores detectables por Flake8.

---
+++ programa4_micros.py
@@ -0,0 +1,4 @@
+# Codigo con errores detectables por Flake8
+lado = int(input("Digite el lado del cuadrado: "))
+Area = lado * lado  # Aqui se calcula el área
+print("El area de un cuadrado de calcula como lado*lado y da:", Area)
