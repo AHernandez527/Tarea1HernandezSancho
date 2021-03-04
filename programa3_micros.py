d39fd89aff3eebc4c5556c42f59ec93701f7fb14
AHernandez527 <79866739+AHernandez527@users.noreply.github.com>
2021-03-04 01:27:26 -0600

    Delete programa3_micros.py

--- programa3_micros.py
+++
@@ -1,8 +0,0 @@
-  # Codigo con errores detectables por Flake8
-
-lado=int  (input("Digite el lado del cuadrado: "))
-Area=lado*lado#Aqui se calcula el área
-print("El area de un cuadrado de calcula elevando al cuadrado el valor del lado, es decir, multiplicándolo por sí mismo, lo que da un resultado de:", Area)
-
-
-    
